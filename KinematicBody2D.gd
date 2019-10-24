e1313c0e864154cc629406ba5ce3a2a399faa50c
Vasuka <vasy2001@yandex.ru>
2019-10-24 23:20:36 +0300

    Добавлена база 2d платформера
    Добавленно базовое управлеение, гравитация и стены

---
+++ KinematicBody2D.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+var motion = Vector2()
+var speed = int()
+var g = int()
+var jump = int();
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const UP = Vector2(0,-1);
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	speed=400;
+	g=30;
+	jump=700;
+	pass # Replace with function body.
+func _physics_process(delta):
+	                #Управление
+	     #Вправо
+	if Input.is_action_pressed("ui_right"):
+		motion.x=speed;
+	     #Влево
+	elif Input.is_action_pressed("ui_left"):
+		motion.x=-speed;
+		
+	else: motion.x=0;
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y=-jump;
+	else:
+		motion.y+=g; #Ускорение падения
+		
+	motion = move_and_slide(motion,UP);#Гравитация
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
